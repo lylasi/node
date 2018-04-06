f17ed8d1898044daac180802a11f1d80abd0ca3d
Linus <Linus@LinustekiMacBook-Air.local>
2018-04-07 02:02:15 +0800

    first

---
+++ config/default.js
@@ -0,0 +1,16 @@
+const config={
+    //port set
+    port:3000,
+
+    //database set
+    database:{
+        DATABASE:'nodesql',
+        USERNAME:'root',
+        PASSWORD:'qwer1234',
+        PORT:'3306',
+        HOST:'localhost',
+        SOCKETPATH:'/Applications/MAMP/tmp/mysql/mysql.sock'
+    }
+}
+
+module.exports=config
